996c7af9b61b738a1e1c931b9dc18c154a2b4b76
3top1a <57371001+3top1a@users.noreply.github.com>
2020-04-14 19:36:30 +0200

    Added Test

---
+++ Code/Test.py
@@ -0,0 +1,13 @@
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("127.0.0.1", 6667))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data)
