757450858ca24b0b74322ef8416c8a4c324ef045
3top1a <57371001+3top1a@users.noreply.github.com>
2020-04-15 14:55:42 +0200

    Primitive shell

--- Code/Classes.py
+++ Code/Classes.py
@@ -1,3 +1,49 @@
-import string, os
+import socket
+import threading
 
+
+class Agent:
+    IP = "127.0.0.1"
+    Port = 6667
+
+    s = socket()
+
+    PositionX = None
+    PositionY = None
+    PositionZ = None
+
+    def start():
+        s.bind((IP, Port))
+        s.listen()
+    
+    def stop():
+        pass
+    
+    def run():
+        conn, addr = s.accept()
+        while(true):
+            data = conn.recv(1024)
+            if not data:
+                break
+            ParseData(data)
+    
+    def ParseData(data):
+        pass
+
+
+
+class AgentManager:
+    Agents = []
+
+
+def connect(ip, port):
+    x = Agent()
+    x.IP = ip
+    x.Port = port
+
+    AgentManager.Agents.append(x)
+
+
+def run():
+    pass
 
--- Code/Main.py
+++ Code/Main.py
@@ -3,6 +3,17 @@ import Classes as classes
 
 
 def __init__():
-    
+    while(True):
+        i = input("$ ")
+
+        if(i.startswith("connect")):
+            ip = i.split(' ')[1]
+            port = i.split(' ')[2]
+
+            print("Connecting to " + ip + " at port " + port)
+            
+            classes.connect(ip, port)
+        if i.startswith("agents"):
+            print("We have " + str(len(classes.AgentManager.Agents)))
 
 __init__()
