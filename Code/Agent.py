b49b4cde9c1ed93eeb23ebcec03b4d5af0793eb6
3top1a <57371001+3top1a@users.noreply.github.com>
2020-04-15 21:43:26 +0200

    FIRST WORKING VERSION!

--- Code/Agent.py
+++ Code/Agent.py
@@ -5,9 +5,11 @@ class Agent:
     Ip = ""
     Port = 6667
 
-    def run(shit):
+
+    def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect(("127.0.0.1", 6667))
             while(True):
                 data = s.recv(4096)
-                print(data)
+                
+                #parsing
