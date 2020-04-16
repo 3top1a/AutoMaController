061142e3f72f15f60550fae4b9b4be24aae209a0
3top1a <57371001+3top1a@users.noreply.github.com>
2020-04-16 15:39:18 +0200

    better readouts

--- Code/Agent.py
+++ Code/Agent.py
@@ -1,9 +1,13 @@
-import socket, time
+import socket, time, re
 
 class Agent:
 
     Ip = ""
     Port = 6667
+
+    Status = 0
+    # 0 = main menu
+    # 1 = in-game
 
 
     def run(self):
@@ -11,5 +15,22 @@ class Agent:
             s.connect(("127.0.0.1", 6667))
             while(True):
                 data = s.recv(4096)
-                
+
+                if not data:
+                    break
+
                 #parsing
+
+                data = str(data).split(' ')
+
+                if(len(data) < 2):
+                    self.Status = 0
+                    return
+                else:
+                    self.Status = 1
+
+                btr_data = ""
+
+                btr_data += ("Position X: " + data[1] + " Y: " + data[2] + "Z:" + data[3])
+
+                print(btr_data)
