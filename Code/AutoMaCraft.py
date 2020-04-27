8d87905a47dda5bb0ceeb012af5a7aa114553100
3top1a <57371001+3top1a@users.noreply.github.com>
2020-04-27 15:51:15 +0200

    lmao dying inside

---
+++ Code/AutoMaCraft.py
@@ -0,0 +1,14 @@
+import GUI as gui
+
+import tkinter as tk
+import tkinter.ttk
+
+
+
+def __init__():
+    app = gui.GUI()
+
+    for i in range(100000):
+        print(i)
+
+__init__()
---
+++ Code/GUI.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import threading
+
+class GUI(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.start()
+
+    def callback(self):
+        self.root.quit()
+
+    def run(self):
+        self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+
+        label = tk.Label(self.root, text="Hello World")
+        label.pack()
+
+        self.root.mainloop()
